cddda5c32b7229be39a15b7707fcc3c50e4e2416
Shahar Bar <33932594+shaharbar1@users.noreply.github.com>
2024-09-26 11:41:03 +0300

    Refactor MAB and Strategy Classes with Cold Start Methods and Enhanced Validation (#35)
    ### Changes
     * Moved Strategy, Model, and MAB to strategy.py, model.py, and to the new mab.py. base.py is now only for definitions and abstract PyBanditsBaseModel. The abstract MAB now allows for all childs to either accept strategy instance as parameter, or to get the strategy parameters and instantiate correspondingly.
     * The from_state functionality is now directly inherited by all MABs from BaseMab.
     * Replaced all cold_start methods in cmab.py and smab.py with cold_start stemming from BaseMab. Correspondingly, updated test cases to use the new cold_start_instantiate methods.
     * Introduced numerize_field and get_expected_value_from_state methods in the Strategy class to handle default values and state extraction. Added field_validator for exploit_p in BestActionIdentification and subsidy_factor in CostControlBandit to ensure proper default handling and validation.
     * Merged common functionality into a new CostControlStrategy abstract class, which is now inherited by CostControlBandit and MultiObjectiveCostControlBandit. Simplified the select_action methods by using helper methods like _evaluate_and_select and _reduce.
     * Plugged get_pareto_front into a new MultiObjectiveStrategy abstract class, which is now inherited by MultiObjectiveBandit and MultiObjectiveCostControlBandit.
     * In model.py. Removed the redundant BaseBetaMO and BaseBayesianLogisticRegression. Added cold_start_instantiate method to BetaMO and BayesianLogisticRegression models.
     * Added extract_argument_names_from_function under utils.py to allow extract function parameter names by handle.
     * Changed test_base.py into test_mab.py.
     * Updated deprecated linter settings in pyproject.toml.
     * Added test_smab_mo_cc_update test on test_smab.py.
     * Changed version to 1.0.0 on pyproject.toml.

--- pybandits/strategy.py
+++ pybandits/strategy.py
@@ -20,15 +20,58 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from abc import ABC, abstractmethod
 from random import random
-from typing import Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 import numpy as np
-from pydantic import validate_call
+from pydantic import field_validator, validate_call
 from scipy.stats import ttest_ind_from_stats
+from typing_extensions import Self
 
-from pybandits.base import ActionId, Float01, Model, Probability, Strategy
-from pybandits.model import Beta, BetaMOCC
+from pybandits.base import ActionId, Float01, Probability, PyBanditsBaseModel
+from pybandits.model import Beta, BetaMOCC, Model
+
+
+class Strategy(PyBanditsBaseModel, ABC):
+    """
+    Strategy to select actions in multi-armed bandits.
+    """
+
+    def _with_argument(self, argument_name: str, argument_value: Any) -> Self:
+        """
+        Instantiate a mutated strategy with an altered argument_value for argument_name.
+
+        Parameters
+        ----------
+        argument_name: str
+            The name of the argument.
+        argument_value: Any
+            The value of the argument.
+
+        Returns
+        -------
+        mutated_strategy: Strategy
+            The mutated strategy.
+        """
+        mutated_strategy = self.model_copy(update={argument_name: argument_value}, deep=True)
+        return mutated_strategy
+
+    @abstractmethod
+    def select_action(self, p: Dict[ActionId, Probability], actions: Optional[Dict[ActionId, Model]]) -> ActionId:
+        """
+        Select the action.
+        """
+
+    @classmethod
+    @validate_call
+    def numerize_field(cls, v, field_name: str):
+        return v if v is not None else cls.model_fields[field_name].default
+
+    @classmethod
+    @validate_call
+    def get_expected_value_from_state(cls, state: Dict[str, Any], field_name: str) -> float:
+        return cls.numerize_field(state["strategy"].get(field_name), field_name)
 
 
 class ClassicBandit(Strategy):
@@ -53,8 +96,10 @@ class ClassicBandit(Strategy):
 
         Parameters
         ----------
-        p: Dict[ActionId, Probability]
-            The dictionary or actions and their sampled probability of getting a positive reward.
+        p : Dict[ActionId, Probability]
+            The dictionary of actions and their sampled probability of getting a positive reward.
+        actions : Optional[Dict[ActionId, Model]]
+            The dictionary of actions and their associated model.
 
         Returns
         -------
@@ -64,7 +109,7 @@ class ClassicBandit(Strategy):
         return max(p, key=p.get)
 
 
-class BestActionIdentification(Strategy):
+class BestActionIdentificationBandit(Strategy):
     """
     Best-Action Identification (BAI) strategy for multi-armed bandits.
 
@@ -73,32 +118,44 @@ class BestActionIdentification(Strategy):
 
     Parameters
     ----------
-    exploit_p: Float_0_1 (default=0.5)
+    exploit_p: Optional[Float01], 0.5 if not specified
         Tuning parameter taking value in [0, 1] which specifies the probability of selecting the best or an alternative
         action.
-        If exploit_p is 1, the bandits always selects the action with highest probability of getting a positive reward,
-            (it behaves as a Greedy strategy).
-        If exploit_p is 0, the bandits always select the action with 2nd highest probability of getting a positive
+        If exploit_p is 1, the bandit always selects the action with the highest probability of
+            getting a positive reward. That is, it behaves as a Greedy strategy.
+        If exploit_p is 0, the bandit always select the action with 2nd highest probability of getting a positive
             reward.
     """
 
-    exploit_p: Float01 = 0.5
+    exploit_p: Optional[Float01] = 0.5
+
+    @field_validator("exploit_p", mode="before")
+    @classmethod
+    def numerize_exploit_p(cls, v):
+        return cls.numerize_field(v, "exploit_p")
 
     @validate_call
-    def set_exploit_p(self, exploit_p: Float01):
+    def with_exploit_p(self, exploit_p: Optional[Float01]) -> Self:
         """
-        Set exploit_p.
+        Instantiate a mutated cost control bandit strategy with an altered subsidy factor.
 
         Parameters
         ----------
-        exploit_p: Float_0_1 (default=0.5)
-            Number in [0, 1] which specifies the amount of exploitation.
-            If exploit_p is 1, the bandits always selects the action with highest probability of getting a positive
-            reward (it behaves as a Greedy strategy).
-            If exploit_p is 0, the bandits always select the action with 2nd highest probability of getting a positive
+        exploit_p: Optional[Float01], 0.5 if not specified
+            Tuning parameter taking value in [0, 1] which specifies the probability of selecting the best or an alternative
+            action.
+            If exploit_p is 1, the bandit always selects the action with the highest probability of
+                getting a positive reward. That is, it behaves as a Greedy strategy.
+            If exploit_p is 0, the bandit always select the action with 2nd highest probability of getting a positive
                 reward.
+
+        Returns
+        -------
+        mutated_best_action_identification : BestActionIdentificationBandit
+            The mutated best action identification strategy.
         """
-        self.exploit_p = exploit_p
+        mutated_best_action_identification = self._with_argument("exploit_p", exploit_p)
+        return mutated_best_action_identification
 
     @validate_call
     def select_action(
@@ -113,8 +170,10 @@ class BestActionIdentification(Strategy):
 
         Parameters
         ----------
-        p: Dict[ActionId, Probability]
-            The dictionary or actions and their sampled probability of getting a positive reward.
+        p : Dict[ActionId, Probability]
+            The dictionary of actions and their sampled probability of getting a positive reward.
+        actions : Optional[Dict[ActionId, Model]]
+            The dictionary of actions and their associated model.
 
         Returns
         -------
@@ -167,7 +226,56 @@ class BestActionIdentification(Strategy):
         return pvalue
 
 
-class CostControlBandit(Strategy):
+class CostControlStrategy(Strategy, ABC):
+    """
+    Cost Control (CC) strategy for multi-armed bandits.
+
+    Bandits are extended to include a control of the action cost. Each action is associated with a predefined "cost".
+    """
+
+    @classmethod
+    @validate_call
+    def _average(cls, p_of_action: Union[Probability, List[Probability]]):
+        return np.mean(p_of_action)
+
+    @classmethod
+    @validate_call
+    def _evaluate_and_select(
+        cls,
+        p: Union[Dict[ActionId, Probability], Dict[ActionId, List[Probability]]],
+        actions: Dict[ActionId, Model],
+        feasible_actions: List[ActionId],
+    ) -> ActionId:
+        """
+        Evaluate the feasible actions and select the one with the minimum cost.
+
+        Parameters
+        ----------
+        p: Union[Dict[ActionId, Probability], Dict[ActionId, List[Probability]]]
+            The dictionary of actions and their sampled probability of getting a positive reward.
+        actions: Dict[ActionId, Model]
+            The dictionary of actions and their associated model.
+        feasible_actions: List[ActionId]
+            The list of feasible actions.
+
+        Returns
+        -------
+        selected_action: ActionId
+            The selected action.
+        """
+        # feasible actions enriched with their characteristics (cost, np.mean(probabilities), action_id)
+        # the negative probability ensures that if we order the actions based on their minimum values the one with
+        # higher probability will be selected
+        sortable_actions = [(actions[a].cost, -cls._average(p[a]), a) for a in feasible_actions]
+
+        # select the action with the min cost (and the highest mean of probabilities in case of cost equality)
+        _, _, selected_action = sorted(sortable_actions)[0]
+
+        # return cheapest action from the set of feasible actions
+        return selected_action
+
+
+class CostControlBandit(CostControlStrategy):
     """
     Cost Control (CC) strategy for multi-armed bandits.
 
@@ -185,18 +293,40 @@ class CostControlBandit(Strategy):
 
     Parameters
     ----------
-    subsidy_factor: Optional[Float_0_1], default=0.5
+    subsidy_factor: Optional[Float01], 0.5 if not specified
         Number in [0, 1] to define smallest tolerated probability reward, hence the set of feasible actions.
         If subsidy_factor is 1, the bandits always selects the action with the minimum cost.
         If subsidy_factor is 0, the bandits always selects the action with highest probability of getting a positive
             reward (it behaves as a classic Bernoulli bandit).
     """
 
-    subsidy_factor: Float01 = 0.5
+    subsidy_factor: Optional[Float01] = 0.5
+
+    @field_validator("subsidy_factor", mode="before")
+    @classmethod
+    def numerize_subsidy_factor(cls, v):
+        return cls.numerize_field(v, "subsidy_factor")
 
     @validate_call
-    def set_subsidy_factor(self, subsidy_factor: Float01):
-        self.subsidy_factor = subsidy_factor
+    def with_subsidy_factor(self, subsidy_factor: Optional[Float01]) -> Self:
+        """
+        Instantiate a mutated cost control bandit strategy with an altered subsidy factor.
+
+        Parameters
+        ----------
+        subsidy_factor : Optional[Float01], 0.5 if not specified
+            Number in [0, 1] to define smallest tolerated probability reward, hence the set of feasible actions.
+            If subsidy_factor is 1, the bandits always selects the action with the minimum cost.
+            If subsidy_factor is 0, the bandits always selects the action with highest probability of getting a positive
+                reward (it behaves as a classic Bernoulli bandit).
+
+        Returns
+        -------
+        mutated_cost_control_bandit : CostControlBandit
+            The mutated cost control bandit strategy.
+        """
+        mutated_cost_control_bandit = self._with_argument("subsidy_factor", subsidy_factor)
+        return mutated_cost_control_bandit
 
     @validate_call
     def select_action(self, p: Dict[ActionId, Probability], actions: Dict[ActionId, Model]) -> ActionId:
@@ -223,65 +353,63 @@ class CostControlBandit(Strategy):
         # define the set of feasible actions
         feasible_actions = [a for a in p.keys() if p[a] >= (1 - self.subsidy_factor) * max_p]
 
-        # feasible actions enriched with their characteristics (cost, -probability, action_id)
-        # the negative probability ensures that if we order the actions based on their minimum values the one with
-        # higher proba will be selected
-        sortable_actions = [(actions[a].cost, -p[a], a) for a in feasible_actions]
-
-        # select the action with the cheapest cost (and the highest probability in case of cost equality)
-        _, _, selected_action = sorted(sortable_actions)[0]
-
-        # return cheapest action from the set of feasible actions
+        selected_action = self._evaluate_and_select(p, actions, feasible_actions)
         return selected_action
 
 
-@validate_call
-def get_pareto_front(p: Dict[ActionId, List[Probability]]) -> List[ActionId]:
+class MultiObjectiveStrategy(Strategy, ABC):
     """
-    Create Pareto optimal set of actions (Pareto front) A* identified as actions that are not dominated by any action
-    out of the set A*.
-
-    Parameters:
-    -----------
-    p: Dict[ActionId, Probability]
-        The dictionary or actions and their sampled probability of getting a positive reward for each objective.
-
-    Return
-    ------
-    pareto_front: set
-        The list of Pareto optimal actions
+    Multi Objective Strategy to select actions in multi-armed bandits.
     """
-    # store non dominated actions
-    pareto_front = []
 
-    for this_action in p.keys():
-        is_pareto = True  # we assume that action is Pareto Optimal until proven otherwise
-        other_actions = [a for a in p.keys() if a != this_action]
+    @classmethod
+    @validate_call
+    def get_pareto_front(cls, p: Dict[ActionId, List[Probability]]) -> List[ActionId]:
+        """
+        Create Pareto optimal set of actions (Pareto front) A* identified as actions that are not dominated by
+        any action out of the set A*.
 
-        for other_action in other_actions:
-            # check if this_action is not dominated by other_action based on
-            # multiple objectives reward prob vectors
-            is_dominated = not (
-                # an action cannot be dominated by an identical one
-                (p[this_action] == p[other_action])
-                # otherwise, apply the classical definition
-                or any(p[this_action][i] > p[other_action][i] for i in range(len(p[this_action])))
-            )
+        Parameters:
+        -----------
+        p: Dict[ActionId, Probability]
+            The dictionary or actions and their sampled probability of getting a positive reward for each objective.
 
-            if is_dominated:
-                # this_action dominated by at least one other_action,
-                # this_action is not pareto optimal
-                is_pareto = False
-                break
+        Return
+        ------
+        pareto_front: set
+            The list of Pareto optimal actions
+        """
+        # store non dominated actions
+        pareto_front = []
 
-        if is_pareto:
-            # this_action is pareto optimal
-            pareto_front.append(this_action)
+        for this_action in p.keys():
+            is_pareto = True  # we assume that action is Pareto Optimal until proven otherwise
+            other_actions = [a for a in p.keys() if a != this_action]
 
-    return pareto_front
+            for other_action in other_actions:
+                # check if this_action is not dominated by other_action based on
+                # multiple objectives reward prob vectors
+                is_dominated = not (
+                    # an action cannot be dominated by an identical one
+                    (p[this_action] == p[other_action])
+                    # otherwise, apply the classical definition
+                    or any(p[this_action][i] > p[other_action][i] for i in range(len(p[this_action])))
+                )
+
+                if is_dominated:
+                    # this_action dominated by at least one other_action,
+                    # this_action is not pareto optimal
+                    is_pareto = False
+                    break
+
+            if is_pareto:
+                # this_action is pareto optimal
+                pareto_front.append(this_action)
+
+        return pareto_front
 
 
-class MultiObjectiveBandit(Strategy):
+class MultiObjectiveBandit(MultiObjectiveStrategy):
     """
     Multi-Objective (MO) strategy for multi-armed bandits.
 
@@ -292,11 +420,6 @@ class MultiObjectiveBandit(Strategy):
 
     Reference: Thompson Sampling for Multi-Objective Multi-Armed Bandits Problem (Yahyaa and Manderick, 2015)
                https://www.researchgate.net/publication/272823659_Thompson_Sampling_for_Multi-Objective_Multi-Armed_Bandits_Problem
-
-    Parameters
-    ----------
-    n_objectives: int
-        Number of objectives to be solved by the bandit (n_objectives must be >= 1).
     """
 
     @validate_call
@@ -316,20 +439,15 @@ class MultiObjectiveBandit(Strategy):
         selected_action: ActionId
             The selected action.
         """
-        return np.random.choice(get_pareto_front(p=p))
+        return np.random.choice(self.get_pareto_front(p=p))
 
 
-class MultiObjectiveCostControlBandit(Strategy):
+class MultiObjectiveCostControlBandit(MultiObjectiveStrategy, CostControlStrategy):
     """
     Multi-Objective (MO) with Cost Control (CC) strategy for multi-armed bandits.
 
     This strategy allows the reward to be a multidimensional vector and include a control of the action cost. It merges
     the Multi-Objective and Cost Control strategies.
-
-    Parameters
-    ----------
-    n_objectives: int
-        Number of objectives to be solved by the bandit (n_objectives must be >= 1)
     """
 
     @validate_call
@@ -349,13 +467,7 @@ class MultiObjectiveCostControlBandit(Strategy):
         selected_action: ActionId
             The selected action.
         """
-        pareto_set = get_pareto_front(p=p)
+        pareto_set = self.get_pareto_front(p=p)
 
-        # feasible actions enriched with their characteristics (cost, np.mean(probabilities), action_id)
-        sortable_actions = [(actions[a].cost, -np.mean(p[a]), a) for a in pareto_set]
-
-        # select the action with the min cost (and the highest mean of probabilities in case of cost equality)
-        _, _, selected_action = sorted(sortable_actions)[0]
-
-        # return cheapest action from the set of feasible actions
+        selected_action = self._evaluate_and_select(p, actions, pareto_set)
         return selected_action
