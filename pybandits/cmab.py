2922d9a8168c44e43d3d2fe44c4624c3204efbce
ronshi <ronshi@playtika.com>
2025-01-14 13:26:14 +0100

    WIP sample_proba

--- pybandits/cmab.py
+++ pybandits/cmab.py
@@ -272,3 +272,154 @@ class CmabBernoulliCC(BaseCmabBernoulli):
     strategy: CostControlBandit
     predict_with_proba: bool = True
     predict_actions_randomly: bool = False
+
+class QuantCmabBernoulli(BaseMab):
+    """
+    Base model for a Contextual Multi-Armed Bandit for Bernoulli bandits with Thompson Sampling.
+
+    Parameters
+    ----------
+    actions: Dict[ActionId, BayesianLogisticRegression]
+        The list of possible actions, and their associated Model.
+    strategy: Strategy
+        The strategy used to select actions.
+    predict_with_proba: bool
+        If True predict with sampled probabilities, else predict with weighted sums.
+    predict_actions_randomly: bool
+        If True predict actions randomly (where each action has equal probability to be selected), else predict with the
+        bandit strategy.
+    """
+
+    actions: Dict[ActionId, BayesianLogisticRegression]
+    predict_with_proba: bool
+    predict_actions_randomly: bool
+
+    @field_validator("actions", mode="after")
+    @classmethod
+    def check_bayesian_logistic_regression_models(cls, v):
+        action_models = list(v.values())
+        first_action = action_models[0]
+        first_action_type = type(first_action)
+        for action in action_models[1:]:
+            if not isinstance(action, first_action_type):
+                raise AttributeError("All actions should follow the same type.")
+            if not len(action.betas) == len(first_action.betas):
+                raise AttributeError("All actions should have the same number of betas.")
+            if not action.update_method == first_action.update_method:
+                raise AttributeError("All actions should have the same update method.")
+            if not action.update_kwargs == first_action.update_kwargs:
+                raise AttributeError("All actions should have the same update kwargs.")
+        return v
+
+    @validate_call(config=dict(arbitrary_types_allowed=True))
+    def predict(
+        self,
+        context: ArrayLike,
+        forbidden_actions: Optional[Set[ActionId]] = None,
+    ) -> CmabPredictions:
+        """
+        Predict actions.
+
+        Parameters
+        ----------
+        context: ArrayLike of shape (n_samples, n_features)
+            Matrix of contextual features.
+        forbidden_actions : Optional[Set[ActionId]], default=None
+            Set of forbidden actions. If specified, the model will discard the forbidden_actions and it will only
+            consider the remaining allowed_actions. By default, the model considers all actions as allowed_actions.
+            Note that: actions = allowed_actions U forbidden_actions.
+
+        Returns
+        -------
+        actions: List[ActionId] of shape (n_samples,)
+            The actions selected by the multi-armed bandit model.
+        probs: List[Dict[ActionId, Probability]] of shape (n_samples,)
+            The probabilities of getting a positive reward for each action.
+        ws : List[Dict[ActionId, float]]
+            The weighted sum of logistic regression logits.
+        """
+        valid_actions = self._get_valid_actions(forbidden_actions)
+
+        # cast inputs to numpy arrays to facilitate their manipulation
+        context = array(context)
+
+        if len(context) < 1:
+            raise AttributeError("Context must have at least one row")
+
+        if self.predict_actions_randomly:
+            # check that context has the expected number of columns
+            if context.shape[1] != len(list(self.actions.values())[0].betas):
+                raise AttributeError("Context must have {n_betas} columns")
+
+            selected_actions = choice(list(valid_actions), size=len(context)).tolist()  # predict actions randomly
+            probs = len(context) * [{k: 0.5 for k in valid_actions}]  # all probs are set to 0.5
+            weighted_sums = len(context) * [{k: 0 for k in valid_actions}]  # all weighted sum are set to 1
+        else:
+            # p is a dict of the sampled probability "prob" and weighted_sum "ws", e.g.
+            #
+            # p = {'a1': ([0.5, 0.2, 0.3], [200, 100, 130]), 'a2': ([0.4, 0.5, 0.6], [180, 200, 230]), ...}
+            #               |               |                           |               |
+            #              prob             ws                          prob            ws
+            p = {
+                action: model.sample_proba(context=context)  # sample probabilities for the entire context matrix
+                for action, model in self.actions.items()
+                if action in valid_actions
+            }
+
+            prob = {a: x[0] for a, x in p.items()}  # e.g. prob = {'a1': [0.5, 0.4, ...], 'a2': [0.4, 0.3, ...], ...}
+            ws = {a: x[1] for a, x in p.items()}  # e.g. ws = {'a1': [200, 100, ...], 'a2': [100, 50, ...], ...}
+
+            # select either "prob" or "ws" to use as input argument in select_actions()
+            p_to_select_action = prob if self.predict_with_proba else ws
+
+            # predict actions, probs, weighted_sums
+            selected_actions = [
+                self._select_epsilon_greedy_action(
+                    p={a: p_to_select_action[a][i] for a in p_to_select_action}, actions=self.actions
+                )
+                for i in range(len(context))
+            ]
+            probs = [{a: prob[a][i] for a in prob} for i in range(len(context))]
+            weighted_sums = [{a: ws[a][i] for a in ws} for i in range(len(context))]
+
+        return selected_actions, probs, weighted_sums
+
+    @validate_call(config=dict(arbitrary_types_allowed=True))
+    def update(
+        self, context: ArrayLike, actions: List[ActionId], rewards: List[Union[BinaryReward, List[BinaryReward]]]
+    ):
+        """
+        Update the contextual Bernoulli bandit given the list of selected actions and their corresponding binary
+        rewards.
+
+        Parameters
+        ----------
+        context: ArrayLike of shape (n_samples, n_features)
+            Matrix of contextual features.
+        actions : List[ActionId] of shape (n_samples,), e.g. ['a1', 'a2', 'a3', 'a4', 'a5']
+            The selected action for each sample.
+        rewards : List[Union[BinaryReward, List[BinaryReward]]] of shape (n_samples, n_objectives)
+            The binary reward for each sample.
+                If strategy is not MultiObjectiveBandit, rewards should be a list, e.g.
+                    rewards = [1, 0, 1, 1, 1, ...]
+                If strategy is MultiObjectiveBandit, rewards should be a list of list, e.g. (with n_objectives=2):
+                    rewards = [[1, 1], [1, 0], [1, 1], [1, 0], [1, 1], ...]
+        """
+        self._validate_update_params(actions=actions, rewards=rewards)
+        if len(context) != len(rewards):
+            raise AttributeError(f"Shape mismatch: actions and rewards should have the same length {len(actions)}.")
+
+        # cast inputs to numpy arrays to facilitate their manipulation
+        context, actions, rewards = array(context), array(actions), array(rewards)
+
+        for a in set(actions):
+            # get context and rewards of the samples associated to action a
+            context_of_a = context[actions == a]
+            rewards_of_a = rewards[actions == a].tolist()
+
+            # update model associated to action a
+            self.actions[a].update(context=context_of_a, rewards=rewards_of_a)
+
+        # always set predict_actions_randomly after update
+        self.predict_actions_randomly = False
+
--- pybandits/model.py
+++ pybandits/model.py
@@ -22,7 +22,7 @@
 import warnings
 from abc import ABC, abstractmethod
 from random import betavariate
-from typing import Any, List, Literal, Optional, Tuple, Union
+from typing import Any, List, Literal, Optional, Tuple, Union, Dict
 
 import numpy as np
 import pymc.math as pmath
@@ -563,3 +563,338 @@ class BayesianLogisticRegressionCC(BayesianLogisticRegression):
     """
 
     cost: NonNegativeFloat
+
+class Quantitative_Cmab(Model):
+    """
+    Base Bayesian Logistic Regression model.
+
+    It is modeled as:
+
+        y = sigmoid(alpha + beta1 * x1 + beta2 * x2 + ... + betaN * xN)
+
+    where the alpha and betas coefficients are Student's t-distributions.
+
+    Parameters
+    ----------
+    alpha : StudentT
+        Student's t-distribution of the alpha coefficient.
+    betas : StudentT
+        Student's t-distributions of the betas coefficients.
+    update_method : UpdateMethods, defaults to "MCMC"
+        The strategy for computing posterior quantities of the Bayesian models in the update function. Such as Markov
+        chain Monte Carlo ("MCMC") or Variational Inference ("VI"). Check UpdateMethods in pybandits.model for the
+        full list.
+    update_kwargs : Optional[dict], uses default values if not specified
+        Additional arguments to pass to the update method.
+    """
+
+    alpha: StudentT
+    actions_to_polynom_order_dict: Dict[str, PositiveInt]
+    if pydantic_version == PYDANTIC_VERSION_1:
+        betas: List[StudentT] = Field(..., min_items=1)
+    elif pydantic_version == PYDANTIC_VERSION_2:
+        betas: List[StudentT] = Field(..., min_length=1)
+    else:
+        raise ValueError("Invalid version.")
+    update_method: UpdateMethods = "MCMC"
+    update_kwargs: Optional[dict] = None
+    _default_update_kwargs = dict(draws=1000, progressbar=False, return_inferencedata=False)
+    _default_mcmc_kwargs = dict(
+        tune=500,
+        draws=1000,
+        chains=2,
+        init="adapt_diag",
+        cores=1,
+        target_accept=0.95,
+        progressbar=False,
+        return_inferencedata=False,
+    )
+    _default_variational_inference_kwargs = dict(method="advi")
+
+    if pydantic_version == PYDANTIC_VERSION_1:
+
+        @model_validator(mode="before")
+        @classmethod
+        def arrange_update_kwargs(cls, values):
+            update_kwargs = cls._get_value_with_default("update_kwargs", values)
+            update_method = cls._get_value_with_default("update_method", values)
+            if update_kwargs is None:
+                update_kwargs = cls._default_update_kwargs
+            if update_method == "VI":
+                update_kwargs = {**cls._default_variational_inference_kwargs, **update_kwargs}
+            elif update_method == "MCMC":
+                update_kwargs = {**cls._default_mcmc_kwargs, **update_kwargs}
+            else:
+                raise ValueError("Invalid update method.")
+            values["update_kwargs"] = update_kwargs
+            values["update_method"] = update_method
+            return values
+
+    elif pydantic_version == PYDANTIC_VERSION_2:
+
+        @model_validator(mode="after")
+        def arrange_update_kwargs(self):
+            if self.update_kwargs is None:
+                self.update_kwargs = self._default_update_kwargs
+            if self.update_method == "VI":
+                self.update_kwargs = {**self._default_variational_inference_kwargs, **self.update_kwargs}
+            elif self.update_method == "MCMC":
+                self.update_kwargs = {**self._default_mcmc_kwargs, **self.update_kwargs}
+            else:
+                raise ValueError("Invalid update method.")
+            return self
+
+    else:
+        raise ValueError(f"Unsupported pydantic version: {pydantic_version}")
+
+    @classmethod
+    def _stable_sigmoid(cls, x: Union[np.ndarray, TensorVariable]) -> Union[np.ndarray, TensorVariable]:
+        """
+        Vectorized sigmoid function that avoids overflow and underflow.
+        Compatible with both numpy and PyMC3 tensors.
+
+        Parameters
+        ----------
+        x : Union[np.ndarray, TensorVariable]
+            Input values.
+
+        Returns
+        -------
+        prob : Union[np.ndarray, TensorVariable]
+            Sigmoid function applied to the input values.
+        """
+        backend = np if isinstance(x, np.ndarray) else pmath
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", RuntimeWarning)
+            prob = backend.where(x >= 0, 1 / (1 + backend.exp(-x)), backend.exp(x) / (1 + backend.exp(x)))
+        return prob
+
+    @validate_call(config=dict(arbitrary_types_allowed=True))
+    def check_context_matrix(self, context: ArrayLike):
+        """
+        Check and cast context matrix.
+
+        Parameters
+        ----------
+        context : ArrayLike of shape (n_samples, n_features)
+            Matrix of contextual features.
+
+        Returns
+        -------
+        context : pandas DataFrame of shape (n_samples, n_features)
+            Matrix of contextual features.
+        """
+        if len(self.actions_to_polynom_order_dict) > 0:
+            num_action_params = sum(self.actions_to_polynom_order_dict.values())
+        else:
+            num_action_params = 0
+
+        try:
+            n_cols_context = array(context).shape[1]
+        except Exception as e:
+            raise AttributeError(f"Context must be an ArrayLike with {len(self.betas) - num_action_params} columns: {e}.")
+
+
+
+        if (n_cols_context + num_action_params) != len(self.betas):
+            raise AttributeError(f"Shape mismatch: context must have {len(self.betas) - num_action_params} columns.")
+
+    @validate_call(config=dict(arbitrary_types_allowed=True))
+    def sample_proba(self, context: ArrayLike) -> Tuple[Probability, float]:
+        """
+        Compute the probability of getting a positive reward from the sampled regression coefficients and the context.
+
+        Parameters
+        ----------
+        context : ArrayLike
+            Context matrix of shape (n_samples, n_features).
+
+        Returns
+        -------
+        prob: ndarray of shape (n_samples)
+            Probability of getting a positive reward.
+        weighted_sum: ndarray of shape (n_samples)
+            Weighted sums between contextual feature values and sampled coefficients.
+        """
+
+        # check input args
+        self.check_context_matrix(context=context)
+
+        # extend context with a column of 1 to handle the dot product with the intercept
+        context_ext = c_[ones((len(context), 1)), context]
+
+        # set number of parameters
+        self.actions_to_polynom_order_dict
+
+
+        alpha = t.rvs(df=self.alpha.nu, loc=self.alpha.mu, scale=self.alpha.sigma, size=len(context_ext))
+        betas = array(
+            [
+                t.rvs(df=self.betas[i].nu, loc=self.betas[i].mu, scale=self.betas[i].sigma, size=len(context_ext))
+                for i in range(len(self.betas))
+            ]
+        )
+
+        # optimize actions polynomials
+        import sympy as sp
+        num_action_quant_actions = len(self.actions_to_polynom_order_dict)
+        quant_values = sp.symbols(f'x_0:{num_action_quant_actions}')
+        num_actions_params = sum(self.actions_to_polynom_order_dict.values())
+        action_betas = betas[:,0:num_actions_params]
+
+        polynomials = 0
+        for i, (_, polynom_order) in enumerate(self.actions_to_polynom_order_dict.items()):
+            for j in range(polynom_order):
+                polynomials += action_betas[:,i + j] * quant_values[i]**(j + 1)
+
+        # calculate the gradient of the polynomial
+        actions_list = []
+        for polynomial in polynomials:
+            gradient = [sp.diff(polynomial, x) for x in quant_values]
+            critical_points = sp.solve(gradient, quant_values)
+
+            # find maximum value:
+            if len(critical_points) == 0:
+                max_critical_point = None
+            else:
+                max_value = -np.inf
+                for critical_point in critical_points:
+                    values_dict = {x: critical_point[i] for i,x in enumerate(quant_values)}
+                    print(polynomial.subs(values_dict))
+                    if polynomial.subs(values_dict) > max_value:
+                        max_critical_point = critical_point
+
+            actions_list.append(max_critical_point)
+
+
+
+
+        # create coefficients matrix
+        coeff = insert(arr=betas, obj=0, values=alpha, axis=0)
+
+
+        # extract the weighted sum between the context and the coefficients
+        weighted_sum = multiply(context_ext, coeff.T).sum(axis=1)
+
+        # compute the probability with the sigmoid function
+        prob = self._stable_sigmoid(weighted_sum)
+
+        return prob, weighted_sum
+
+    @validate_call(config=dict(arbitrary_types_allowed=True))
+    def update(self, context: ArrayLike, rewards: List[BinaryReward]):
+        """
+        Update the model parameters.
+
+        Parameters
+        ----------
+        context : ArrayLike
+            Context matrix of shape (n_samples, n_features).
+        rewards: List[BinaryReward]
+            A list of binary rewards.
+        """
+
+        # check input args
+        self.check_context_matrix(context=context)
+        if len(context) != len(rewards):
+            AttributeError("Shape mismatch: context and rewards must have the same length.")
+
+        with PymcModel() as _:
+            # update intercept (alpha) and coefficients (betas)
+            # if model was never updated priors_parameters = default arguments
+            # else priors_parameters are calculated from traces of the previous update
+            alpha = PymcStudentT("alpha", mu=self.alpha.mu, sigma=self.alpha.sigma, nu=self.alpha.nu)
+            beta_mu = [b.mu for b in self.betas]
+            beta_sigma = [b.sigma for b in self.betas]
+            beta_nu = [b.nu for b in self.betas]
+            betas = PymcStudentT("betas", mu=beta_mu, sigma=beta_sigma, nu=beta_nu, shape=len(self.betas))
+
+            context = Data("context", context, mutable=False)
+            rewards = Data("rewards", rewards, mutable=False)
+
+            # Likelihood (sampling distribution) of observations
+            weighted_sum = Deterministic("weighted_sum", alpha + dot(betas, context.T))
+            p = Deterministic("p", self._stable_sigmoid(weighted_sum))
+
+            # Bernoulli random vector with probability of success given by sigmoid function and actual data as observed
+            _ = Bernoulli("likelihood", p=p, observed=rewards)
+
+            # update traces object by sampling from posterior distribution
+            if self.update_method == "VI":
+                # variational inference
+                update_kwargs = self.update_kwargs.copy()
+                approx = fit(method=update_kwargs.pop("method"))
+                trace = approx.sample(**update_kwargs)
+            elif self.update_method == "MCMC":
+                # MCMC
+                trace = sample(**self.update_kwargs)
+            else:
+                raise ValueError("Invalid update method.")
+
+            # compute mean and std of the coefficients distributions
+            self.alpha.mu = mean(trace["alpha"])
+            self.alpha.sigma = std(trace["alpha"], ddof=1)
+            betas_mu = mean(trace["betas"], axis=0)
+            betas_std = std(trace["betas"], axis=0, ddof=1)
+            self.betas = [
+                StudentT(mu=mu, sigma=sigma, nu=beta.nu) for mu, sigma, beta in zip(betas_mu, betas_std, self.betas)
+            ]
+
+    @classmethod
+    def cold_start(
+        cls,
+        n_params: PositiveInt,
+        update_method: UpdateMethods = "MCMC",
+        update_kwargs: Optional[dict] = None,
+        **kwargs,
+    ) -> "BayesianLogisticRegression":
+        """
+        Utility function to create a Bayesian Logistic Regression model  or child model with cost control,
+        with default parameters.
+
+        It is modeled as:
+
+            y = sigmoid(alpha + beta1 * x1 + beta2 * x2 + ... + betaN * xN)
+
+        where the alpha and betas coefficients are Student's t-distributions.
+
+        Parameters
+        ----------
+        n_params : PositiveInt
+            The number of betas of the Bayesian Logistic Regression model. This is also the number of features expected
+            after in the context matrix.
+        update_method : UpdateMethods, defaults to "MCMC"
+            The strategy for computing posterior quantities of the Bayesian models in the update function. Such as Markov
+            chain Monte Carlo ("MCMC") or Variational Inference ("VI"). Check UpdateMethods in pybandits.model for the
+            full list.
+        update_kwargs : Optional[dict], uses default values if not specified
+            Additional arguments to pass to the update method.
+        kwargs: Dict[str, Any]
+            Additional arguments for the Bayesian Logistic Regression child model.
+
+        Returns
+        -------
+        blr: BayesianLogisticRegrssion
+            The Bayesian Logistic Regression model.
+        """
+        return cls(
+            alpha=StudentT(),
+            betas=[StudentT() for _ in range(n_params)],
+            update_method=update_method,
+            update_kwargs=update_kwargs,
+            **kwargs,
+        )
+
+if __name__ == '__main__':
+    n_samples = 10
+    n_features = 5
+    actions_to_polynom_order_dict = {'a': 2, 'b': 3}
+    num_actions_params = sum(actions_to_polynom_order_dict.values())
+
+
+    blr = Quantitative_Cmab.cold_start(n_params=n_features + num_actions_params,  actions_to_polynom_order_dict=actions_to_polynom_order_dict)
+
+    # context is numpy array
+    context = np.random.uniform(low=-100.0, high=100.0, size=(n_samples, n_features))
+    assert type(context) is np.ndarray
+    prob, weighted_sum = blr.sample_proba(context=context)
